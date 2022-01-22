889fc84709a2a4a7933dbbd2cc6efaf69f7b3a1d
cheenlam <56752830+cheenlam@users.noreply.github.com>
2022-01-22 12:09:39 +0800

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,19 @@
+let imgList = {
+    mainBg: 'images/bg.jpg',
+    logo: 'images/logo.png',
+    slog: 'images/slog.png'
+}
+
+let dlList = {
+    main: { href: '#', title: '點擊進入' },
+    android: [
+        { href: '#', title: '安卓下載點1' },
+        { href: '#', title: '安卓下載點2' },
+        { href: '#', title: '安卓下載點3' },
+    ],
+    ios: [
+        { href: '#', title: '蘋果下載點1' },
+        { href: '#', title: '蘋果下載點2' },
+        { href: '#', title: '蘋果下載點3' },
+    ]
+};
